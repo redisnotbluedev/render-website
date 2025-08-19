09c9f971ee9bfb767b641ef687f7ecc5abc0c815
quuuut <102846821+quuuut@users.noreply.github.com>
2025-08-19 11:34:36 +0930

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask, redirect
+from flask import Flask, redirect, request
 
 app = Flask(__name__)
 
 @app.route("/")
 def main():
   #return "asd"
-  return redirect("https://redisnotblue.up.railway.app", code=301)
+  return redirect("https://redisnotblue.up.railway.app" + request.path, code=301)
 
 
 app.run()
